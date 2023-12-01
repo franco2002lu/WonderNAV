a48f141bf4143ef61ba0ecd5ff935cb061563fcd
Franco Lu <luji0808@gmail.com>
2023-12-01 17:39:19 -0600

    openAI integration now working, still need to store in DynamoDB

--- rust_app/src/AddChatFunction.rs
+++ rust_app/src/AddChatFunction.rs
@@ -1,10 +1,10 @@
-//use llm_chain::{executor, parameters, prompt, step::Step};
+use async_openai::{types::CreateCompletionRequestArgs, Client as OpenAIClient, config::OpenAIConfig};
 use aws_sdk_dynamodb::{Client, Error as DynamoError};
 use aws_sdk_dynamodb::types::AttributeValue;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
 use aws_config::BehaviorVersion;
-use anyhow::anyhow;
+// use anyhow::anyhow;
 
 #[derive(Deserialize)]
 struct Request {
@@ -33,9 +33,9 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
             })
         },
         Ok(None) => {
+            //todo: need to get put this response into DynamoDB here (the body part)
             Ok(Response {
                 statusCode: 200,
-                //fixme: add logic to call gpt api
                 body: transform_result(generate_response(&request.body).await),
             })
         },
@@ -57,16 +57,31 @@ fn transform_result(result: Result<String, Box<dyn std::error::Error>>) -> Strin
 }
 
 async fn generate_response(input: &str) -> Result<String, Box<dyn std::error::Error>> {
-    // let exec = executor!()?;
-    // let step = Step::for_prompt_template(prompt!(
-    //     "You are an experienced travel agent that will provide an in-depth itinerary based on relevant online articles. You will provide the itinerary based on the location and duration entered by the user. Include at least 3 activities a day. Do not include any other suggestions or comments before or after the itinerary.",
-    //     "{{text}}" // Text is the default parameter name
-    // ));
-    // let res = step.run(&parameters!(input), &exec).await?;
-    // let itinerary_advice: String = res.to_string();
-    // Ok(itinerary_advice)
-    // fixme: suspect that gpt's api messes up the environment
-    Ok("need to generate response and stuff".to_string())
+    //todo: need to implement error handling
+    let api_key = "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";
+    let openai_config = OpenAIConfig::new()
+        .with_api_key(api_key);
+
+    let openai_client = OpenAIClient::with_config(openai_config);
+    let mut openai_prompt = "You are an experienced travel agent that will provide an in-depth itinerary based on relevant online articles. You will provide the itinerary based on the location and duration entered by the user. Include at least 3 activities a day. Do not include any other suggestions or comments before or after the itinerary."
+        .to_string();
+    openai_prompt.push_str(input);
+
+    let openai_request = CreateCompletionRequestArgs::default()
+        .model("text-davinci-003")
+        .prompt(openai_prompt)
+        .max_tokens(900_u16)
+        .build()
+        .unwrap();
+
+    let openai_response = openai_client
+        .completions()
+        .create(openai_request)
+        .await
+        .unwrap();
+
+    let output = &openai_response.choices[0].text;
+    Ok(output.to_string())
 }
 
 async fn query_dynamodb(client: &Client, input: &str) -> Result<Option<String>, DynamoError> {
@@ -83,7 +98,6 @@ async fn query_dynamodb(client: &Client, input: &str) -> Result<Option<String>, 
             match output_attr.as_s() {
                 Ok(output) => Ok(Some(output.to_string())),
                 Err(_) => Ok(None)
-
                 //Err(_) => Ok(Some("Item attribute does not exist".to_string()))
             }
         } else {
